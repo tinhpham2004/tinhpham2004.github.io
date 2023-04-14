df2c08ecb7ee9ea49f3e1dc81a60a7251093a846
tinhpham2004 <boydzpro3032004@gmail.com>
2023-04-14 19:12:06 +0700

    new update

--- js/myJs.js
+++ js/myJs.js
@@ -14,8 +14,8 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'Hello cậu!',
-        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
+        title: 'Xin chào em bé!',
+        text: 'Anh có điều này muốn hỏi em bé nhớ phải trả lời thật lòng nhaaa :>>>',
         imageUrl: '../img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu dễ thương lắm í :<<<<<<< ";
+    var text = " Tại vì anh dễ thương lắm í :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('../sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+        title: 'Nói cho anh lí do em bé thích anh đi ><',
         html: true,
         width: 900,
         padding: '3em',
@@ -121,8 +121,8 @@ $('#yes').click(function() {
                 width: 900,
                 confirmButtonText: 'Okiiiii lun <3',
                 background: '#fff url("../img/iput-bg.jpg")',
-                title: 'Tớ biết mà >< Yêu cậu 3000',
-                text: "Còn chờ gì nữa mà ko ib cho tớ đi nàoooooo!!!!",
+                title: 'Anh ngay biết mà >< Yêu em bé nhiều nhắm',
+                text: "Còn chờ gì nữa mà hỏng ib cho anh đi nàoooooo!!!!",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
                     window.location = 'https://www.facebook.com/tinhpham3032004';
