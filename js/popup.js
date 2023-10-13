a148988831a2648fe94802b0d323a2bf9f6c40dc
tinhpham2004 <boydzpro3032004@gmail.com>
2023-10-13 20:13:16 +0700

    fix btn animation

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,12 @@
 Swal.fire({
-    title: 'Custom width, padding, background.',
-    width: 600,
-    padding: '3em',
-    background: '#fff url(/images/trees.png)',
-    backdrop: `
+  title: "Custom width, padding, background.",
+  width: 600,
+  padding: "3em",
+  background: "#fff url(../img/puung2.jpg))",
+  backdrop: `
       rgba(0,0,123,0.4)
-      url("/images/nyan-cat.gif")
+      url("../images/giphy2.gif")
       left top
       no-repeat
-    `
-  })
+    `,
+});
